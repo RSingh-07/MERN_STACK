6136a8cbfb53b841cecf3885b7420a940885b27f
Riya Singh <537riya@gmail.com>
2025-07-16 10:07:33 +0530

    commented previous files

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const riya = require("./second");  {/*whatever secondjs is giving bring it to harry*/}
+// Importing the object from 'second.js' using require()
+// 'riya' now holds whatever was exported from 'second.js'
+const riya = require("./second");  {/*whatever secondjs is giving bring it to riya*/}
 
+
+//Logging a greeting and the imported object
 console.log("Hello Girl!", riya)
--- second.js
+++ second.js
@@ -1,7 +1,9 @@
+// simple object that holds user info
 riya = {
     name: "Riya",
     favNum: 7,
     developer: true
 }
 
+//Exporting the object so other files can use it
 module.exports = riya;
